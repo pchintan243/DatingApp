28b3829abbd899f2582267ff6a65f7d6f1fe3573
chintan <patelchintan843@gmail.com>
2024-01-13 15:25:09 +0530

    14 Creating the connection string

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -15,11 +15,9 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController()
     {
-        _logger = logger;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
