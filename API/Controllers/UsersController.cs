962e01ebe2ca2b91f0fe5c817307c38d85b5f28f
chintan <patelchintan843@gmail.com>
2024-01-16 10:39:35 +0530

    16 Adding API Controller endpoints

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("{id}")] // api/users/2
+
+    public ActionResult<AppUser> GetUser(int id) {
+        var user = _context.Users.Find(id);
+        return user;
+    }
+}
