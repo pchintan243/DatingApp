38b60d6ce8d59225b1bd7d93ffe7ae9767da5985
chintan <patelchintan843@gmail.com>
2024-01-16 18:21:02 +0530

    37: register API endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace API;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public AccountController(DataContext context) {
+        _context = context;
+    }
+
+    [HttpPost("register")] //POST: api/account/register?username=patel&password=pwd
+
+    public async Task<ActionResult<AppUser>> Register(string username, string password) {
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+}
