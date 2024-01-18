42cb22bcc2c6d62390cb1eacbd3c1af41006172e
chintan <patelchintan843@gmail.com>
2024-01-18 17:21:57 +0530

    57: localstorage login concept

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,16 +1,28 @@
 import { AccountService } from './../_services/account.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
   model: any = {}
   loggedIn = false;
   constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe({
+      next: user => this.loggedIn = !!user,
+      error: error => console.log(error)
+      
+    })
+  }
 
   login() {
     this.accountService.login(this.model).subscribe({
@@ -23,6 +35,7 @@ export class NavComponent {
   }
 
   logout() {
+    this.accountService.logout();
     this.loggedIn = false;
   }
 }
