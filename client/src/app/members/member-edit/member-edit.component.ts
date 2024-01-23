71fab2a343505849ee7158f8b472a845542438ff
chintan <patelchintan843@gmail.com>
2024-01-23 16:12:18 +0530

    Section:10::: 117: Implemented member-edit component

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,34 @@
+import { MembersService } from './../../_services/members.service';
+import { AccountService } from './../../_services/account.service';
+import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { take } from 'rxjs';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  member: Member | undefined;
+  user: User | null = null;
+
+  constructor(private accountService: AccountService, private membersService: MembersService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => this.user = user
+    })
+  }
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    if (!this.user) return;
+    this.membersService.getMember(this.user.username).subscribe({
+      next: member => this.member = member
+    })
+  }
+
+
+}
