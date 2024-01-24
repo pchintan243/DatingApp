d3216aff7949f2c460ab29e9b91e0393f6c051dc
chintan <patelchintan843@gmail.com>
2024-01-24 13:37:34 +0530

    123: loading indicator

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxSpinner, NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [],
@@ -11,11 +12,15 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     ToastrModule.forRoot(),
+    NgxSpinnerModule.forRoot({
+      type: 'square-loader'
+    })
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
-    TabsModule
+    TabsModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
