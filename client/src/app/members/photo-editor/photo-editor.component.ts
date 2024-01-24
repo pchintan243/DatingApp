190c215bebd1dbd673d5f069f21ea1c06788d3ce
chintan <patelchintan843@gmail.com>
2024-01-24 18:44:48 +0530

    133: Added Photo Editor Component

---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,12 @@
+import { Member } from 'src/app/_models/member';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent {
+  @Input() member: Member | undefined;
+  
+}
