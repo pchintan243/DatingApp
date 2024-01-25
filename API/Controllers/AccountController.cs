5d1f55266c775ddac3bd2d0ba4692c08872b3c6d
chintan <patelchintan843@gmail.com>
2024-01-25 12:27:05 +0530

    137: Added User photo in nav

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,7 +53,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(
                 x => x.UserName == loginDto.Username);
 
             if (user == null)
@@ -70,7 +73,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
