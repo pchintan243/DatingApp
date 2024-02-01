1bd63b5f42f02ac3e259caec4c6ddcf4db77c198
chintan <patelchintan843@gmail.com>
2024-02-01 15:02:34 +0530

    163: Implemented LastActive user filter

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var username = resultContext.HttpContext.User.GetUserName();
+
+            var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+
+            var user = await repo.GetUserByUsernameAsync(username);
+
+            user.LastActive = DateTime.UtcNow;
+            await repo.SaveAllAsync();
+        }
+    }
+}
