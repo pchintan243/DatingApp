25dd7bccc5203e97c897ea63c26af73e8e2771f9
chintan <patelchintan843@gmail.com>
2024-02-01 15:20:15 +0530

    164: Optimize fetching data by id

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -15,11 +15,11 @@ namespace API.Helpers
 
             if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
-            var username = resultContext.HttpContext.User.GetUserName();
+            var userId = resultContext.HttpContext.User.GetUserId();
 
             var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
 
-            var user = await repo.GetUserByUsernameAsync(username);
+            var user = await repo.GetUserByIdAsync(int.Parse(userId));
 
             user.LastActive = DateTime.UtcNow;
             await repo.SaveAllAsync();
