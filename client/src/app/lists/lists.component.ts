04e9605557f363ee7effc1119b12df720bafbe31
chintan <patelchintan843@gmail.com>
2024-02-02 11:56:46 +0530

    177: A like component

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { MembersService } from './../_services/members.service';
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css']
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit{
+  members: Member[] | undefined;
+  predicate = 'liked';
 
+  constructor(private membersService: MembersService) { }
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes(){
+    this.membersService.getLikes(this.predicate).subscribe({
+      next: response => {
+        this.members = response
+      }
+    })
+  }
 }
