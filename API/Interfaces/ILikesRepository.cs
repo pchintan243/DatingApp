fd31cd797e5b3b5ef5688cb988173d513d34394e
chintan <patelchintan843@gmail.com>
2024-02-02 10:34:32 +0530

    173: Added Like repository

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate);
+    }
+}
