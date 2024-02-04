5add2204668bf853c417fbc16711f4ffe8defd58
chintan <patelchintan843@gmail.com>
2024-02-04 18:40:11 +0530

    191: Stying message thread

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { MessageService } from './../../_services/message.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Message } from 'src/app/_models/message';
+import { TimeagoModule } from 'ngx-timeago';
 
 @Component({
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, TimeagoModule]
 })
 export class MemberMessagesComponent implements OnInit {
 
