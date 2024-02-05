7123aae349514edc2813261ec2c21130b7409cef
chintan <patelchintan843@gmail.com>
2024-02-05 13:09:43 +0530

    Section16:::  201: S up the entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -36,11 +36,7 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -63,15 +59,6 @@ namespace API.Controllers
 
             if (user == null)
                 return Unauthorized("Invalid Username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i])
-                    return Unauthorized("Invalid Password");
-            }
 
             return new UserDto
             {
