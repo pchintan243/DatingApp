169abbebcc9da71f1328a8c1ed976b5131d65e66
chintan <patelchintan843@gmail.com>
2024-02-05 15:47:55 +0530

    209: Policy based Authorization

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+        [Authorize(Policy = "RequireAdminRole" )]
+        [HttpGet("users-with-roles")]
+        public ActionResult GetUserWithRoles()
+        {
+            return Ok("Only admins can see this");
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole" )]
+        [HttpGet("photos-to-moderate")]
+        public ActionResult GetPhotosForModeration()
+        {
+            return Ok("Admins or Moderators can see this");
+        }
+
+    }
+}
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -13,13 +13,14 @@ namespace API.Extensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddIdentityCore<AppUser>(opt => {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
                 opt.Password.RequireNonAlphanumeric = false;
             })
             .AddRoles<AppRole>()
             .AddRoleManager<RoleManager<AppRole>>()
             .AddEntityFrameworkStores<DataContext>();
-            
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
                 options.TokenValidationParameters = new TokenValidationParameters
@@ -30,6 +31,13 @@ namespace API.Extensions
                     ValidateAudience = false
                 };
             });
+
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
             return services;
         }
 
