ff4814712b89ce1be7c7b718db1ab6c01927d018
chintan <patelchintan843@gmail.com>
2024-02-06 10:38:14 +0530

    223: Client side singalR

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,3 +1,4 @@
+import { PresenceService } from './presence.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
@@ -13,7 +14,8 @@ export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient, private presenceService: PresenceService) { }
 
   login(model: any) {
 
@@ -43,11 +45,13 @@ export class AccountService {
     Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
+    this.presenceService.createHubConnection(user);
   }
 
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+    this.presenceService.stopHubConnection();
   }
 
   getDecodedToken(token: string) {
